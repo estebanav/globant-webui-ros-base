044a8f8b08828892cbd27758d033cfe79d79a2db
Bernardo Raúl Cases <bernardo.cases@globant.com>
2014-10-24 22:10:07 -0300

    firebase example 1

--- js/main.js
+++ js/main.js
@@ -1,46 +1,96 @@
-var Hackton = Hackton || {};
+var Message = {
+    name: '',
+    text:'',
+    timestamp: Date.now(),
+    getDate: function(){
+        var date = new Date(this.timestamp);
+        return date.toLocaleString();
+    },
+    getHTMLMessage: function(){
+        var $msg = $('<div/>');
+        $msg.text(this.text + ' - ');
+        $msg.prepend($('<em/>').text(this.name + ': '));
+        $msg.prepend($('<span/>').text(this.getDate() + ' - '));
+        $msg.append($('<a href="#" />').addClass('js-btn-delete').text('Remove'));
+        
+        return $msg;
+    }
+};
 
-Hackton.init = function () {
-    Hackton._data = new Firebase('https://glb-hackton.firebaseio.com/');
-    $('#js-txt-message').keypress(function (e) {
-        if (e.keyCode == 13) {
-            var text = $('#js-txt-message').val();
-            var name = $('#js-txt-name').val();
-            var timestamp = Date.now();
+var dataSource = new Firebase('https://glb-hackton.firebaseio.com/');
 
-            Hackton._data.push({name: name, text: text, timestamp: timestamp});
-            $('#js-txt-message').val('');
-        }
-    });
-    $('#js-messages').on('click', '.js-btn-delete', function (e) {
-        e.preventDefault();
-        var id = $(this).parents('[data-message-id]').data('message-id');
-        Hackton._data.child(id).remove();
-    });
-    Hackton._data.on('child_added', function (snapshot) {
-        displayMessage(snapshot);
-    });
 
-    Hackton._data.on('child_removed', function (snapshot) {
-        removeMessage(snapshot);
-    });
+var Hackton = (function () {
+
+    function init() {
+        initData();
+        bindEvents();
+    }
+    
+    function initData() {
+        dataSource.on('child_added', function (snapshot) {
+            displayMessage(snapshot);
+        });
+
+        dataSource.on('child_removed', function (snapshot) {
+            removeMessage(snapshot);
+        });
+    }
+    
+    
+    function bindEvents() {
+        
+        $('#js-txt-message').keypress(function (e) {
+            if (e.keyCode == 13) {
+                var text = $('#js-txt-message').val();
+                var name = $('#js-txt-name').val();
+                $('#js-txt-message').val('');
+                
+                
+                var message = Object.create(Message);
+                message.name = name;
+                message.text = text;
+                message.timestamp = Date.now();
+                
+                dataSource.push(message);
+                
+            }
+        });
+        
+        
+        $('#js-messages').on('click', '.js-btn-delete', function (e) {
+            e.preventDefault();
+            var id = $(this).parents('[data-message-id]').data('message-id');
+            dataSource.child(id).remove();
+        });
+    }
+
+
 
     function displayMessage(snapshot) {
         var $el = $('<div/>');
         $el.attr('data-message-id', snapshot.name());
 
         var message = snapshot.val();
-        var date = new Date(message.timestamp);
-        $('<div/>').text(message.text).prepend($('<em/>').text(message.name + ': ')).prepend($('<span/>').text(date.toLocaleString() + ' - ')).appendTo($el);
-        $('<a href="#" />').addClass('js-btn-delete').text('Remove').appendTo($el);
+        
+        
+        $el.append(message.getHTMLMessage());
+        
         $el.appendTo($('#js-messages'));
-        $('#js-messages')[0].scrollTop = $('#js-messages')[0].scrollHeight;
+        
     }
+    
+    
     function removeMessage(snapshot) {
-        $('[data-message-id=' + snapshot.name()+']').slideUp(function () {
+        $('[data-message-id=' + snapshot.name() + ']').fadeOut(function () {
             $(this).remove();
         });
     }
-};
+    
+    return{
+        init: init
+    };
+
+})();
 
 Hackton.init();
